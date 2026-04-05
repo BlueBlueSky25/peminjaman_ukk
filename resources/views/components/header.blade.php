725a5ff3f96bfa25348f55e26dfdef6d7224eedf
BlueBlueSky25 <lutfika44@gmail.com>
2026-04-05 16:39:23 +0700

    perubahan warna

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-white shadow-sm border-b-4 border-blue-500 sticky top-0 z-40">
+<header class="bg-white shadow-sm border-b-4 border-red-500 sticky top-0 z-40">
     <div class="max-w-7xl mx-auto px-3 sm:px-4 lg:px-8 py-2.5 sm:py-3.5 flex justify-between items-center gap-2">
         <!-- Logo & Title -->
         <div class="flex items-center space-x-2 min-w-0 flex-1">
